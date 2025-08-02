90412ebc0a46e622ac877c7cb71165806a9052b9
Exityx <gdlasorum@gmail.com>
2025-08-02 12:20:47 +0300

    Edit src/veeboo.cpp

---
+++ src/veeboo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+const std::string VEEBOO_TITLE_ASCII_ART = R"(
+__     __        ____              
+\ \   / /__  ___| __ )  ___   ___  
+ \ \ / / _ \/ _ \  _ \ / _ \ / _ \ 
+  \ V /  __/  __/ |_) | (_) | (_) |
+   \_/ \___|\___|____/ \___/ \___/ 
+)";
+
+int main(void)
+{
+    std::cout << "VeeBoo - ";
+    return 0;
+}
+
